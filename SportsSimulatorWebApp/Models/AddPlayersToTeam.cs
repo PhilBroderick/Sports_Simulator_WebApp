83d0b0abdab61031ce920861e7ac9fb3178d6be3
philbroderick12@gmail.com <philbroderick12@gmail.com>
2018-09-03 20:41:36 +0100

    Starting to sort out League Teams and Team Players table views. Need to work more on the design.

---
+++ SportsSimulatorWebApp/Models/AddPlayersToTeam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsSimulatorWebApp.Models
+{
+    public class AddPlayersToTeam
+    {
+        public string TeamName { set; get; }
+        public int TeamId { set; get; }
+        public Team team { get; set; }
+        public int PlayerId { get; set; }
+        public int[] PlayerIds { set; get; }
+        public List<SelectListItem> Players { set; get; }
+    }
+}
