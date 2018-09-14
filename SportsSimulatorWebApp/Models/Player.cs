6b06d9f0b3a8fac74284ed9f15973482d6a980c8
philbroderick12@gmail.com <philbroderick12@gmail.com>
2018-09-14 18:22:09 +0100

    Added new fields to team and player model.

--- SportsSimulatorWebApp/Models/Player.cs
+++ SportsSimulatorWebApp/Models/Player.cs
@@ -25,6 +25,8 @@ namespace SportsSimulatorWebApp.Models
         public string LastName { get; set; }
         public decimal PlayerRating { get; set; }
         public string Position { get; set; }
+        public Nullable<decimal> AttackRating { get; set; }
+        public Nullable<decimal> DefenseRating { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<TeamMember> TeamMembers { get; set; }
