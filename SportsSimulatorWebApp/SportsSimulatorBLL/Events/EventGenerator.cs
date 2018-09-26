079dfdcb87d6050d5befec4525860a55556eb37e
philbroderick12@gmail.com <philbroderick12@gmail.com>
2018-09-26 21:59:46 +0100

    Now returning a List<TeamEvents> from the Event generator class. I need to figure out how to add this to a List<List<Enum>> in the Event Generator Manager.

--- SportsSimulatorWebApp/SportsSimulatorBLL/Events/EventGenerator.cs
+++ SportsSimulatorWebApp/SportsSimulatorBLL/Events/EventGenerator.cs
@@ -13,20 +13,20 @@ namespace SportsSimulatorWebApp.SportsSimulatorBLL.Events
         //    GenerateEvent(matchup);
         //}
 
-        public TeamEvents GenerateEvent(Matchup matchup)
+        public List<TeamEvents> GenerateEvent(Matchup matchup)
         {
-            var eventOutcome = GenerateRandomEvent(matchup);
+            List<TeamEvents> eventOutcome = GenerateRandomEvent(matchup);
 
             return eventOutcome;
         }
-        public enum TeamEvents { Attack, Defend, Scrum, Lineout, TryHome, TryAway, DropGoal };
-        public enum SubsequentEvents { Try, Conversion}
+        public enum TeamEvents { Attack, Defend, Scrum, Lineout, TryHome, TryAway, DropGoal , Conversion };
 
-        static TeamEvents GenerateRandomEvent(Matchup matchup)
+        static List<TeamEvents> GenerateRandomEvent(Matchup matchup)
         {
             var random = new Random();
             var randomEvent = (TeamEvents)random.Next(0, 7); //returns a random event from the TeamEvents enum
-
+            List<TeamEvents> Events = new List<TeamEvents>();
+            TeamEvents subsequentEvent;
             bool subsequentAction = false;
             
             switch (randomEvent)
@@ -58,7 +58,8 @@ namespace SportsSimulatorWebApp.SportsSimulatorBLL.Events
                 case TeamEvents.DropGoal:
                     DropGoalEvent dropGoal = new DropGoalEvent();
                     subsequentAction = dropGoal.PlayEvent(matchup);
-                    return randomEvent;
+                    Events.Add(randomEvent);
+                    return Events;
 
             }
 
@@ -69,55 +70,90 @@ namespace SportsSimulatorWebApp.SportsSimulatorBLL.Events
                 {
                     ConversionEvent subSequentConversion = new ConversionEvent();
                     bool isConversion = subSequentConversion.PlayEvent(matchup);
-                    return randomEvent;
+                    subsequentEvent = TeamEvents.Conversion;
+                    Events.Add(randomEvent);
+                    Events.Add(subsequentEvent);
+                    return Events;
                 }
                 else if(randomEvent == TeamEvents.Defend)
                 {
                     TryAwayEvent subsequentAwayTry = new TryAwayEvent();
                     bool isAwayTry = subsequentAwayTry.PlayEvent(matchup);
+                    Events.Add(randomEvent);
                     if (isAwayTry)
                     {
                         ConversionEvent awayConversion = new ConversionEvent();
                         bool isConversion = awayConversion.PlayEvent(matchup);
-                        return randomEvent;
+                        subsequentEvent = TeamEvents.Conversion;
+                        Events.Add(subsequentEvent);
+                        return Events;
                     }
-                    return randomEvent;
+                    return Events;
                 }
                 else if(randomEvent == TeamEvents.TryAway)
                 {
                    TryAwayEvent subsequentAwayTryAttempt = new TryAwayEvent();
                    bool isAwayTry = subsequentAwayTryAttempt.PlayEvent(matchup);
-                
-                   if (isAwayTry)
+                   Events.Add(randomEvent);
+                    if (isAwayTry)
                    {
                        ConversionEvent awayConversion = new ConversionEvent();
                        bool isConversion = awayConversion.PlayEvent(matchup);
-                       return randomEvent;
-                   }
-                   return randomEvent;
+                       subsequentEvent = TeamEvents.Conversion;
+                       Events.Add(subsequentEvent);
+                       return Events;
+                    }
+                   return Events;
                 }
                 else if(randomEvent == TeamEvents.DropGoal)
                 {
-                    return randomEvent;
+                    return Events;
                 }
                 else
                 {
                     TryHomeEvent subSequentHomeTry = new TryHomeEvent();
                     bool isHomeTry = subSequentHomeTry.PlayEvent(matchup);
+                    Events.Add(randomEvent);
                     if (isHomeTry)
                     {
                        ConversionEvent homeConversion = new ConversionEvent();
                        bool isConversion = homeConversion.PlayEvent(matchup);
-                       return randomEvent;
+                       subsequentEvent = TeamEvents.Conversion;
+                       Events.Add(subsequentEvent);
+                       return Events;
                     }
-                    return randomEvent;
+                    return Events;
                 }
             }
             else
             {
-                return randomEvent;
+                Events.Add(randomEvent);
+                return Events;
             }
             
+        }
+    }
+
+    static class EnumHelpers<T> where T : struct
+    {
+        public class NotAnEnumException : Exception
+        {
+            public NotAnEnumException() : base(string.Format(@"Type ""{0}"" is not an Enum type.", typeof(T))) { }
+        }
+
+        static EnumHelpers()
+        {
+            if (typeof(T).BaseType != typeof(Enum)) throw new NotAnEnumException();
+        }
+
+        public static IEnumerable<T> GetValues()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+
+        public static T Parse(string value)
+        {
+            return (T)Enum.Parse(typeof(T), value);
         }
     }
 }
