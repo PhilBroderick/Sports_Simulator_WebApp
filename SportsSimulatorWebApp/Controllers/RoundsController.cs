d71d24f3c29b87435612021ca884de7bb35742d0
Phil Broderick <phil.broderick@forthcom.co.uk>
2018-10-04 12:55:33 +0100

    Starting to add all rounds to rounds controller. Need to add hover over functionality so hovering over a round shows all the matchups in that round.

---
+++ SportsSimulatorWebApp/Controllers/RoundsController.cs
@@ -0,0 +1,31 @@
+﻿using SportsSimulatorWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsSimulatorWebApp.Controllers
+{
+    public class RoundsController : Controller
+    {
+        private SportsSimulatorDBEntities db = new SportsSimulatorDBEntities();
+        // GET: Rounds
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult ViewAllRounds(int id)
+        {
+            League league = db.Leagues.Find(id);
+
+            return View();
+        }
+    }
+}
